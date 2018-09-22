d5861b49b5275f87c1587f281f517b0adb555e9a
Stacy Mackin <stacy.mackin@gmail.com>
2018-09-22 14:11:20 -0400

    feat: Add client and server date validation

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const billboard = require('billboard-top-100').getChart;
 const moment = require('moment');
 const search = require('youtube-search');
-const errMsg = 'chart not found';
+const errMsg = {'chart':{'errpr':'chart not found'}, 'date': {'error': 'invalid date'}};
 const youtubeOpts = {
   maxResults: 1,
   key: process.env.YOUTUBE_KEY
@@ -15,6 +15,7 @@ const getTopTen = (date) => {
   let topTen = [];
   return new Promise((resolve, reject) => {
     billboard('hot-100', date, (err, songs) => {
+      console.log(songs)
       if (err) return reject(err)
       console.log('Getting billboard data...')
       for(let i = 0; i < 10; i++) {
@@ -45,24 +46,29 @@ const getYoutubeLink = (topTen) => {
 };
 
 app.get('/api/date', (req, res) => {
-  let year = req.query.year;
   let month = req.query.month < 10 ? `0${req.query.month}` : req.query.month;
-  let day  = req.query.day < 10 ? `0${req.query.day}` : req.query.day;
+  let day = req.query.day < 10 ? `0${req.query.day}` : req.query.day;
+  let year = req.query.year;
   let date = `${year}-${month}-${day}`;
 
-  console.log(date);
-
-  getTopTen(date).then((topTen) => {
-    Promise.all(getYoutubeLink(topTen)).then((links) => {
-      res.send(links);
-    }).catch(() => {
-      console.log(errMsg);
-      res.send(errMsg);
-    })
-  }).catch((e) => {
-    console.log(errMsg);
-    res.send(errMsg);
-  });
+  // Validate date
+  if (moment(date).isValid() && moment(moment(new Date()).diff(date, 'days')) > 0) {
+    getTopTen(date).then((topTen) => {
+      Promise.all(getYoutubeLink(topTen)).then((links) => {
+        res.send(links);
+      }).catch(() => {
+        console.log(errMsg['chart']);
+        res.send(errMsg['chart']);
+      })
+    }).catch((e) => {
+      console.log(errMsg['chart']);
+      res.send(errMsg['chart']);
+    });
+    console.log(date);
+  }
+  else {
+    res.send(errMsg['date']);
+  }
 });
 
 app.use(express.static('dist'));
--- src/js/main.controller.js
+++ src/js/main.controller.js
@@ -2,16 +2,14 @@
 
 const app = angular.module('youtubeBillboard', ['ngRoute', 'duScroll']);
 
-app.config(($routeProvider, $locationProvider) => {
-  $routeProvider
-  .when('/:month?/:day?/:year?', {
+app.config(($routeProvider, $locationProvider, $sceDelegateProvider) => {
+  $routeProvider.when('/:month?/:day?/:year?', {
     templateUrl : 'templates/main.html',
     controller: 'mainCtrl'
   });
-  $locationProvider.html5Mode(true);
-});
 
-app.config(($sceDelegateProvider) => {
+  // $locationProvider.html5Mode(true);
+
   $sceDelegateProvider.resourceUrlWhitelist([
     'self',                    // trust all resources from the same origin
     '*://www.youtube.com/**'   // trust all resources from `www.youtube.com`
@@ -61,15 +59,21 @@ app.controller('mainCtrl', function($scope, $routeParams, $http, $route, $locati
   });
 
   if ($routeParams.month !== undefined && $routeParams.day !== undefined && $routeParams.year !== undefined) {
-    recordLoader(true);
-    $http.get(`/api/date?month=${$scope.month}&day=${$scope.day}&year=${$scope.year}`)
-    .then(function(topTen){
-      recordLoader(false)
-      $scope.songs = topTen.data;
-    }).catch(function(err){
-      recordLoader(false);
-      alert(`No chart found for ${$scope.month}-${$scope.day}-${$scope.year}`);
-    });
+    if (validateDate($routeParams.month, $routeParams.day, $routeParams.year)) {
+      recordLoader(true);
+      $http.get(`/api/date?month=${$scope.month}&day=${$scope.day}&year=${$scope.year}`)
+      .then(function(topTen){
+        if (topTen.data.hasOwnProperty('error')) throw 'Sorry, no top ten found for that date.';
+        recordLoader(false)
+        $scope.songs = topTen.data;
+      }).catch((e) => {
+        recordLoader(false);
+        alert(e);
+      });
+    }
+    else {
+      alert('Please enter a valid date.')
+    }
   }
 
   $scope.dateSubmit = function(year, month, day){
@@ -77,7 +81,7 @@ app.controller('mainCtrl', function($scope, $routeParams, $http, $route, $locati
     recordLoader(true);
     $http.get(`/api/date?month=${month}&day=${day}&year=${year}`)
     .then(function(topTen){
-      if (topTen.data === 'chart not found') throw 'Sorry, no top ten found for that date.';
+      if (topTen.data.hasOwnProperty('error')) throw 'Sorry, no top ten found for that date.';
       recordLoader(false);
       $scope.songs = topTen.data;
       $route.updateParams({month: $scope.month, day: $scope.day, year: $scope.year});
@@ -87,7 +91,14 @@ app.controller('mainCtrl', function($scope, $routeParams, $http, $route, $locati
     });
   };
 
-  function recordLoader(toggle){
+  function validateDate(month, day, year) {
+    month = month < 10 ? `0${month}` : month;
+    day = day < 10 ? `0${day}` : day;
+    let date = `${year}-${month}-${day}`;
+    return moment(date).isValid() && moment(moment(new Date()).diff(date, 'days')) > 0;
+  }
+
+  function recordLoader(toggle) {
     if (toggle) {
       // angular.element($document.find('#record-loader')).addClass('toggle')
       document.getElementById('record-loader').style.opacity = '1';
@@ -95,14 +106,14 @@ app.controller('mainCtrl', function($scope, $routeParams, $http, $route, $locati
     }
     else {
       document.getElementById('record-loader').style.opacity = '0';
-        setTimeout(() => {
-          document.getElementById('record-loader').style.display = 'none';
-          if (document.getElementById('song1')) {
-            $document.scrollToElement(angular.element(document.getElementById('song1')), 75, 300);
-          }
-        }, 300);
+      setTimeout(() => {
+        document.getElementById('record-loader').style.display = 'none';
+        if (document.getElementById('song1')){
+          $document.scrollToElement(angular.element(document.getElementById('song1')), 75, 300);
+        }
+      }, 300);
     }
-  }
+  };
 
   $scope.toTheTop = function() {
     $document.scrollToElement(angular.element(document.getElementsByTagName('body')[0]), 500, 500);
