91b9a2c93429a07f2f85d2e22f0f01d21c4faf53
Stacy Mackin <stacy.mackin@gmail.com>
2020-04-15 22:23:41 -0400

    Revert

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const app = express();
 const path = require('path');
 require('dotenv').config();
 
-const requireHTTPS = (req, res, next) => {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (
-    !req.secure &&
-    req.get('x-forwarded-proto') !== 'https' &&
-    process.env.NODE_ENV !== 'development'
-  ) {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-};
-app.use(requireHTTPS);
+// const requireHTTPS = (req, res, next) => {
+//   // The 'x-forwarded-proto' check is for Heroku
+//   if (
+//     !req.secure &&
+//     req.get('x-forwarded-proto') !== 'https' &&
+//     process.env.NODE_ENV !== 'development'
+//   ) {
+//     return res.redirect('https://' + req.get('host') + req.url);
+//   }
+//   next();
+// };
+// app.use(requireHTTPS);
 app.use(express.static('dist'));
 app.use('/api/topten/', require('./app/routes/api/topten'));
 app.use('/api/video/', require('./app/routes/api/video'));
